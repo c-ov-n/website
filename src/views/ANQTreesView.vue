3d547a33ed84deeff4cfa2411c0709a1a4ce7cb3
Nia Ain <niaskotl@gmail.com>
2025-06-29 20:15:46 -0400

    Change to antora

--- src/views/ANQTreesView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="anq-trees">
-    <h1>The Trees</h1>
-    <p>Under construction.</p>
-  </div>
-</template>
--- src/views/HomeView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="home">
-    <h1>Thee C-ov-N</h1>
-  </div>
-</template>
