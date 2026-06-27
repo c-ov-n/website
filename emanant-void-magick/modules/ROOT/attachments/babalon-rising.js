f34b6887b1d80688371457370fdfaf073e2b3c07
Nia Ain <niaskotl@gmail.com>
2026-06-27 15:25:59 -0400

    Add babalon rising docs

---
+++ emanant-void-magick/modules/ROOT/attachments/babalon-rising.js
@@ -0,0 +1,403 @@
+const STEPS = [{
+  "name": "title-page",
+  "treeDiagram": {
+    "cp": "t",
+    "fv": 0,
+    "pl": "full-",
+    "s": "emanation-11-31",
+  }
+}, {
+  "name": "evm-what",
+  "treeDiagram": {
+    "cp": "t",
+    "fv": 0,
+    "pl": "full-",
+    "s": "emanation-11-31",
+  }
+}, {
+  "name": "evm-why",
+  "treeDiagram": {
+    "cp": "t",
+    "fv": 0,
+    "pl": "full-",
+    "s": "emanation-11-31",
+  }
+}, {
+  "name": "evm-how",
+  "treeDiagram": {
+    "cp": "t",
+    "fv": 0,
+    "pl": "full-",
+    "s": "emanation-11-31",
+  }
+}, {
+  "name": "narrative-ontology",
+  "treeDiagram": {
+    "cp": "t",
+    "fv": 0,
+    "pl": "full-",
+    "s": "emanation-11-31",
+  }
+}, {
+  "name": "emanation-0",
+  "treeDiagram": {
+    "s": "emanation-0",
+  }
+}, {
+  "name": "emanation-00",
+  "treeDiagram": {
+    "s": "emanation-00",
+  }
+}, {
+  "name": "emanation-000",
+  "treeDiagram": {
+    "s": "emanation-000",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-0-1",
+  "treeDiagram": {
+    "s": "emanation-0-1",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-0-1-element",
+  "treeDiagram": {
+    "s": "emanation-0-1",
+    "sphereIcons": "element",
+  }
+}, {
+  "name": "emanation-1-2",
+  "treeDiagram": {
+    "cp": "f",
+    "fv": 0,
+    "m": "f",
+    "pl": "emanation-",
+    "s": "emanation-1-2",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-1-2-fools-journey",
+  "treeDiagram": {
+    "cp": "f",
+    "fj": "t",
+    "fv": 1,
+    "m": "f",
+    "pl": "full-",
+    "s": "emanation-1-2",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-1-2-element",
+  "treeDiagram": {
+    "cp": "f",
+    "m": "f",
+    "pl": "emanation-",
+    "s": "emanation-1-2",
+    "sphereIcons": "element",
+  }
+}, {
+  "name": "emanation-2-3",
+  "treeDiagram": {
+    "cp": "f",
+    "fv": 0,
+    "m": "f",
+    "pl": "emanation-",
+    "s": "emanation-2-3",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-2-3-fools-journey",
+  "treeDiagram": {
+    "cp": "f",
+    "fj": "t",
+    "fv": 1,
+    "m": "f",
+    "pl": "full-",
+    "s": "emanation-2-3",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-2-3-element",
+  "treeDiagram": {
+    "cp": "f",
+    "m": "f",
+    "pl": "emanation-",
+    "s": "emanation-2-3",
+    "sphereIcons": "element",
+  }
+},{
+  "name": "emanation-3-5",
+  "treeDiagram": {
+    "cp": "f",
+    "m": "f",
+    "pl": "emanation-",
+    "s": "emanation-3-5",
+    "sphereIcons": "sigil",
+  }
+},{
+  "name": "emanation-3-5-element",
+  "treeDiagram": {
+    "cp": "f",
+    "m": "f",
+    "pl": "emanation-",
+    "pn": "decimal",
+    "s": "emanation-3-5",
+    "sphereIcons": "element",
+  }
+}, {
+  "name": "emanation-4-7",
+  "treeDiagram": {
+    "cp": "f",
+    "m": "f",
+    "pl": "emanation-",
+    "s": "emanation-4-7",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-4-7-element",
+  "treeDiagram": {
+    "cp": "t",
+    "m": "t",
+    "pl": "emanation-",
+    "s": "emanation-4-7",
+    "sphereIcons": "symbol-element",
+  }
+}, {
+  "name": "emanation-5-11",
+  "treeDiagram": {
+    "cp": "t",
+    "m": "t",
+    "pl": "emanation-",
+    "s": "emanation-5-11",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-5-11-flip",
+  "treeDiagram": {
+    "cp": "f",
+    "pl": "emanation-",
+    "s": "emanation-5-11",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-5-11-element",
+  "treeDiagram": {
+    "cp": "t",
+    "pl": "emanation-",
+    "s": "emanation-5-11",
+    "sphereIcons": "symbol-element",
+  }
+}, {
+  "name": "emanation-6-13",
+  "treeDiagram": {
+    "cp": "t",
+    "fv": 0,
+    "pl": "emanation-",
+    "s": "emanation-6-13",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-6-13-fools-journey",
+  "treeDiagram": {
+    "cp": "t",
+    "fj": "t",
+    "fv": 1,
+    "pl": "full-",
+    "s": "emanation-6-13",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-7-17",
+  "treeDiagram": {
+    "cp": "t",
+    "pl": "emanation-",
+    "s": "emanation-7-17",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-8-19",
+  "treeDiagram": {
+    "cp": "t",
+    "fv": 0,
+    "pl": "emanation-",
+    "s": "emanation-8-19",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-8-19-fools-journey",
+  "treeDiagram": {
+    "cp": "t",
+    "fj": "t",
+    "fv": 1,
+    "pl": "full-",
+    "s": "emanation-8-19",
+    "sphereIcons": "sigil",
+  }
+}, {
+  "name": "emanation-9-23",
+  "treeDiagram": {
+    "cp": "t",
+    "pl": "emanation-",
+    "s": "emanation-9-23",
+  }
+}, {
+  "name": "emanation-9-23-uncross",
+  "treeDiagram": {
+    "cp": "f",
+    "pl": "emanation-",
+    "s": "emanation-9-23",
+  }
+}, {
+  "name": "emanation-10-29",
+  "treeDiagram": {
+    "cp": "t",
+    "pl": "emanation-",
+    "s": "emanation-10-29",
+  }
+}, {
+  "name": "emanation-11-31",
+  "treeDiagram": {
+    "cp": "t",
+    "fv": 0,
+    "pl": "emanation-",
+    "s": "emanation-11-31",
+  }
+}, {
+  "name": "emanation-11-31-fools-journey",
+  "treeDiagram": {
+    "cp": "t",
+    "fj": "t",
+    "fv": 1,
+    "pl": "full-",
+    "s": "emanation-11-31",
+  }
+}, {
+  "name": "emanation-11-31-explore",
+  "treeDiagram": {
+    "cp": "t",
+    "fj": "t",
+    "fv": 0,
+    "pl": "full-",
+    "nav": "t",
+    "opt": "t",
+    "s": "emanation-11-31",
+  }
+}];
+
+class Emanation {
+  constructor(contentElement, treeDiagramElement) {
+    this.contentElement = contentElement;
+    this.treeDiagramElement = treeDiagramElement;
+    this.nextButton = contentElement.querySelector('.nav > button.next');
+    this.previousButton = contentElement.querySelector('.nav > button.previous');
+    this.pathNumberModeSigilSelect = contentElement.querySelector('.nav > input[type="radio"][value="sigil"]');
+    this.pathNumberModeDecimalSelect = contentElement.querySelector('.nav > input[type="radio"][value="decimal"]');
+
+    window.onhashchange = () => {
+      var hashParams = new URLSearchParams(window.location.hash.substring(1));
+      this.pathNumberMode = hashParams.get("pn") || "sigil";
+      this.setStep(hashParams.get("s"));
+    }
+    if (window.location.hash) {
+      const hashParams = new URLSearchParams(window.location.hash.substring(1));
+      this.pathNumberMode = hashParams.get("pn") || "sigil";
+      if (this.pathNumberMode === "sigil") {
+        this.pathNumberModeSigilSelect.checked = true;
+      } else {
+        this.pathNumberModeDecimalSelect.checked = true;
+      }
+      this.setStep(hashParams.get("s") || STEPS[0].name);
+    } else {
+      window.location.hash = `s=${STEPS[0].name}&pn=sigil`;
+    }
+
+    this.nextButton.onclick = (event) => {
+      event.preventDefault();
+      this.showNext();
+    }
+
+    this.previousButton.onclick = (event) => {
+      event.preventDefault();
+      this.showPrevious();
+    }
+
+    this.pathNumberModeDecimalSelect.onchange = (event) => {
+      this.setPathNumberMode("decimal")
+    }
+    this.pathNumberModeSigilSelect.onchange = (event) => {
+      this.setPathNumberMode("sigil")
+    }
+  }
+
+  setButtonsDisabled() {
+    this.previousButton.disabled = this.step === STEPS[0];
+    this.nextButton.disabled = this.step === STEPS[STEPS.length - 1];
+  }
+
+  setContentClass() {
+    this.contentElement.className = `description ${this.step.name}`;
+  }
+
+  setDiagramHash() {
+    const hashParams = new URLSearchParams();
+    for (const key in this.step.treeDiagram) {
+      if (key === "pl") {
+        hashParams.set(key, this.step.treeDiagram[key] + this.pathNumberMode);
+      } else {
+        hashParams.set(key, this.step.treeDiagram[key]);
+      }
+    }
+    if (this.treeDiagramElement.contentWindow.location == "about:blank") {
+      this.treeDiagramElement.contentWindow.location = `treeanim.html#${hashParams.toString()}`;
+    } else {
+      this.treeDiagramElement.contentWindow.location.hash = hashParams.toString();
+    }
+  }
+
+  setPathNumberMode(mode) {
+    var hashParams = new URLSearchParams(window.location.hash.substring(1));
+    hashParams.set("pn", mode);
+    window.location.hash = hashParams.toString();
+  }
+
+  setStep(stepName) {
+    this.step = STEPS.find((step) => step.name === stepName);
+    this.setButtonsDisabled();
+    this.setDiagramHash();
+    this.setContentClass();
+  }
+
+  setStepInHash(step) {
+    var hashParams = new URLSearchParams(window.location.hash.substring(1));
+    hashParams.set("s", step.name);
+    window.location.hash = hashParams.toString();
+  }
+
+  setNextEmanation() {
+    const n = STEPS.indexOf(this.step);
+    if (n >= STEPS.length - 1) { return }
+    this.setStepInHash(STEPS[n+1]);
+  }
+
+  setPreviousEmanation() {
+    const n = STEPS.indexOf(this.step);
+    if (n == 0) { return }
+    this.setStepInHash(STEPS[n-1]);
+  }
+
+  showNext() {
+    this.setNextEmanation();
+  }
+
+  showPrevious() {
+    this.setPreviousEmanation();
+  }
+}
+
+window.onload = () => {
+  const emanation = new Emanation(
+    document.getElementById('content'),
+    document.getElementById('tree-diagram'),
+  );
+}
